6e1a0601899e4c464ac477401017cc722bf594b6
ThibautLemichel <thibaut.lemichel@student.junia.com>
2025-06-03 17:59:07 +0200

    update addproduct page style

--- src/lib/components/ProductComponent.svelte
+++ src/lib/components/ProductComponent.svelte
@@ -11,6 +11,10 @@
 
 	const dispatch = createEventDispatcher();
 
+	const colors = ['#3490e4', '#ff9675', '#9363ff', '#ff6a73'];
+	// Pick a random color index for each instance
+	const colorIndex = Math.floor(Math.random() * colors.length);
+
 	// When the user clicks anywhere on the container (except the +), navigate
 	function onContainerClick() {
 		goto(`/addproduct/product/${id}`);
@@ -26,12 +30,14 @@
 </script>
 
 <div
-	class="product-component relative cursor-pointer bg-white shadow-md"
+	class="product-component relative cursor-pointer shadow-md"
+	style="background: {colors[colorIndex]}"
+
 	on:click={onContainerClick}
 >
 	<h2 class="text-1xl text-left font-bold">{name}</h2>
 
-	<div class="flex items-center text-sm text-gray-600">
+	<div class="flex items-center text-sm">
 		{#if quantity !== undefined}
 			<span>Quantity: {quantity} g</span>
 		{/if}
@@ -44,12 +50,12 @@
 	</div>
 
 	<div
-		class="absolute top-1/2 right-1.5 -translate-y-1/2 rounded-full bg-blue-500"
+		class="plus absolute top-1/2 right-1.5 -translate-y-1/2 rounded-full"
 		style="margin-right: 5px;"
 		on:click={onPlusClick}
 		aria-label="Add this product to diary"
 	>
-		<img src={plusUrl} alt="Plus" style="width: 24px; height: 24px;" />
+		<img src={plusUrl} alt="Plus" style="width: 35px; padding: 4px;"/>
 	</div>
 </div>
 
@@ -59,9 +65,22 @@
 		padding: 1rem;
 		border-radius: 8px;
 		margin-bottom: 1rem;
-		background: #fafafa;
 	}
 	h2 {
 		margin: 0 0 0.5rem 0;
+		  color: var(--color-white);
 	}
+	.plus {
+border-radius: 50%;
+fill: white;
+background-color: var(--color-contrast);
+opacity: 1;
+ }
+ .plus:hover {
+	transition: ease-in-out 0.2s;
+  	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+ }
+ .product-component div {
+  color: var(--color-contrast);
+ }
 </style>
